40d9b5dc3deb670ea715bf0be99eef208f822a26
ieatprettygirl <dima20030617@mail.ru>
2025-03-30 01:35:04 +0500

    change test port

--- company-vacancy-service/demo/src/test/java/net/javaguides/springboot/CompanyControllerTest.java
+++ company-vacancy-service/demo/src/test/java/net/javaguides/springboot/CompanyControllerTest.java
@@ -31,7 +31,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 
-@SpringBootTest(classes = SpringbootBackendApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Slf4j
 public class CompanyControllerTest {
