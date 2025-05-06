ce70d8c6b048b0402f2d9aed8977b148f39eae7a
CauseImFeeling <depayu52@gmail.com>
2025-05-06 07:25:59 +0500

    удалены тестовые сервисы

--- resume-service/demo/src/main/java/net/javaguides/springboot/repository/ResumeRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springboot.repository;
-
-import net.javaguides.springboot.model.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
