8f130d1ae9cfa8b43efde4a67cb8a0b248cfd126
CauseImFeeling <depayu52@gmail.com>
2025-05-23 19:56:30 +0500

    изменена страницы резюме

--- frontend/my-app/src/app/resume/[id]/page.tsx
+++ frontend/my-app/src/app/resume/[id]/page.tsx
@@ -481,17 +481,17 @@ export default function ResumePage() {
                                                     />
                                                 </div>
 
-                                                <div className="mb-4">
-                                                    <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>
-                                                    <input
-                                                        type="email"
-                                                        name="contact"
-                                                        disabled
-                                                        value={formData.contact || ''}
-                                                        onChange={handleInputChange}
-                                                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"
-                                                    />
-                                                </div>
+                                                {/*<div className="mb-4">*/}
+                                                {/*    <label className="block text-sm font-medium text-gray-700 mb-1">Email</label>*/}
+                                                {/*    <input*/}
+                                                {/*        type="email"*/}
+                                                {/*        name="contact"*/}
+                                                {/*        disabled*/}
+                                                {/*        value={resume.}*/}
+                                                {/*        onChange={handleInputChange}*/}
+                                                {/*        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"*/}
+                                                {/*    />*/}
+                                                {/*</div>*/}
                                             </>
                                         ) : (
                                             <>
